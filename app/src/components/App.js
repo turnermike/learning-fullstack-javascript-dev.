3ae536537b511c2725bdc01f5e19c21dbb0ed0e7
Mike <turner.mike@gmail.com>
2018-12-17 17:09:02 -0500

    state demo was good

---
+++ app/src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from './Header';
+
+
+
+
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+        ...
+        </div>
+      </div>
+    );
+  }
+}
+
+// Stateless Example:
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <Header message="Naming Contests" />
+//       <div>
+//       ...
+//       </div>
+//     </div>
+//   );
+// };
+
+export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -1,46 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-// const color = Math.random() > 0.5 ? 'green' : 'red';
-
-const Header = ({ message }) => {
-  return (
-    <h2 className="text-color">
-      {message}
-    </h2>
-  );
-};
-
-Header.propTypes = {
-  // headerMessage: React.PropTypes.string.isRequired
-  message: PropTypes.string.isRequired
-};
 
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>
-      ...
-      </div>
-    </div>
-  );
-};
+import App from './components/App';
 
 // render method
 ReactDOM.render(
-  // React.createElement('h2', null, 'Hello React'),
-
-  // <h2 className="text-center">
-  //     Centered
-  // </h2>,
-
-  // <h2 style={{color}}>
-  //     Hello React JSX -- {Math.random()}
-  // </h2>,
-
   <App />,
   document.getElementById('root')
 );
