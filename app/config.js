465057f694470f880d67f97b403e97a476fb8441
Mike <turner.mike@gmail.com>
2018-12-17 09:59:51 -0500

    finished 2.1 export examples

--- app/config.js
+++ app/config.js
@@ -1,1 +1,16 @@
 const env = process.env;
+
+// variable export example
+export const nodeEnv = env.NODE_ENV || 'development';
+
+// function export example
+export const logStars = function(message) {
+    console.info('**************************');
+    console.info(message);
+    console.info('**************************');
+};
+
+// default export object
+export default {
+    port: env.port || 8080
+};
--- app/server.js
+++ app/server.js
@@ -1,1 +1,5 @@
-import config from './config';
+import config, { nodeEnv, logStars } from './config';
+
+console.log('config', config);      // default export object
+console.log('nodeEnv', nodeEnv);    // example of a variable export
+logStars('Testing ABC');            // example of a function export
