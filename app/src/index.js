f59b3d11a276aee7b7155be421efe832c99c12d3
Mike <turner.mike@gmail.com>
2018-12-17 14:33:18 -0500

    3.2 done

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'green' : 'red';
+
 ReactDOM.render(
-    React.createElement('h2', null, 'Hello React'),
+    // React.createElement('h2', null, 'Hello React'),
+    <h2 className="text-center">
+        Centered
+    </h2>,
+    // <h2 style={{color}}>
+    //     Hello React JSX -- {Math.random()}
+    // </h2>,
     document.getElementById('root')
 );
+
