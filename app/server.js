b0c615e29b20bebb585b0502fe574cddf0cd3423
Mike <turner.mike@gmail.com>
2018-12-18 11:50:39 -0500

    node-sass-middleware working

--- app/server.js
+++ app/server.js
@@ -1,21 +1,31 @@
 import config, { nodeEnv, logStars } from './config';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 import express from 'express';
 const server = express();
+
+// use node-sass-middleware module
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'compressed'
+}));
+// console.log('path', path.join(__dirname, 'public'));
 
 // use ejs template language (Embeded Javascript)
 // express will look for ejs templates under the 'views' dir
 server.set('view engine', 'ejs');
 
-
+// default route
 server.get('/', (req, res) => {
-    // res.send('Hello Express');
-    res.render('index', {
-        content: 'Hello Express and <em>EJS</em>!'
-    });
+  // res.send('Hello Express');
+  res.render('index', {
+    content: 'Hello Express and <em>EJS</em>!',
+  });
 });
-
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
@@ -30,7 +40,7 @@ server.use(express.static('public'));
 // });
 
 server.listen(config.port, () => {
-    console.log('Express listening on port ', config.port);
+  console.log('Express listening on port ', config.port);
 });
 
 // console.log('config', config);      // default export object
--- app/src/components/App.js
+++ app/src/components/App.js
@@ -2,34 +2,31 @@ import React from 'react';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
-
-
 class App extends React.Component {
   state = {
-    pageHeader: 'Naming Contests'
+    pageHeader: 'Naming Contests',
   };
   componentDidMount() {
     // ajax stuff
     // timers/liseners
-
     // console.log('did Mount');
     // debugger;
   }
   componentWillUnmount() {
     // clean timers/listners here
-
     // console.log('will Unmount');
     // debugger;
   }
   // using '...' spread operator in ContestPreview component so it gets all object properies of contests
   render() {
+
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          {this.props.contests.map(contest =>
-            <ContestPreview {...contest} />
-          )}
+          {this.props.contests.map(contest => (
+            <ContestPreview key="{contest.id}" {...contest} />
+          ))}
         </div>
       </div>
     );
